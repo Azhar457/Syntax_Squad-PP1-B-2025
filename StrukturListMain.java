c52998f12458f8714b6379b93f5e35c0c9c22555
azhar457 <azharsss457@gmail.com>
2025-05-15 09:52:42 +0700

    Upload

---
+++ StrukturListMain.java
@@ -0,0 +1,10 @@
+public class StrukturListMain {
+    public static void main(String[] args) {
+        StrukturList list = new StrukturList();
+        list.addHead(10);
+        list.addTail(20);
+        list.addMid(1,2);
+        list.displayElement();
+
+    }
+}
