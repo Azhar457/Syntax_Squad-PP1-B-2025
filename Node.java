3cb4c8b41103703468e56b2ced28d5585617ade8
azhar457 <azharsss457@gmail.com>
2025-05-19 19:57:47 +0700

    Upload

--- Node.java
+++ Node.java
@@ -1,22 +1,3 @@
-public class Node{
-private int data;
-private Node next;
-public Node(int data){
-    this.data = data;
-    this.next = null;
-}
+public class Node {
 
-public int getData(){
-    return data;
-}
-public void setData(int data){
-    this.data = data;
-}
-public Node getNext(){
-    return next;
-}
-
-public void setNext(Node next){
-    this.next = next;
-}
 }
