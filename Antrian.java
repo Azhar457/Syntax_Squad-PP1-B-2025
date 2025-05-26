044669b654082de47c5d78e8473041eabbf91842
mildakhaerunnisa <mildakhaerunnisa25@gmail.com>
2025-05-26 14:29:04 +0700

    upload

---
+++ Antrian.java
@@ -0,0 +1,38 @@
+import java.util.Date;
+
+public class Antrian { // [NoNota, TglMasuk, TglSelesai, [Nama, berat], Layanan]
+    private String noNota;
+    private Date tglMasuk;
+    private Date tglSelesai;
+    private Pelanggan pelanggan;// [Nama, Berat]
+    private String layanan;
+    // 1 = Reguler, 2 = Express
+
+    public Antrian(String noNota, Date tglMasuk, Date tglSelesai, Pelanggan pelanggan, String layanan) {
+        this.noNota = noNota;
+        this.tglMasuk = tglMasuk;
+        this.tglSelesai = tglSelesai;
+        this.pelanggan = pelanggan;
+        this.layanan = layanan;
+    }
+
+    public String getNoNota() {
+        return noNota;
+    }
+
+    public String getLayanan() {
+        return layanan;
+    }
+
+    public Date getTglMasuk() {
+        return tglMasuk;
+    }
+
+    public Date getTglSelesai() {
+        return tglSelesai;
+    }
+
+    public Pelanggan getPelanggan() {
+        return pelanggan;
+    }
+}
